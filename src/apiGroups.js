aa0cb8c21e28b56507e327c8e463600e12d3d418
Marco Manco <manco.mrc@gamil.com>
2021-11-30 22:39:55 +0100

    feat: add decoration for @netgroup-polito/CrownLabs project

---
+++ src/apiGroups.js
@@ -0,0 +1,9 @@
+// This file serves development/testing purposes only.
+// When qlkube is deployed with Helm, it is overwritten by an equivalent
+// one automatically generated from the configuration therein specified.
+
+const apiGroups = {
+  paths: ['crownlabs.polito.it'],
+};
+
+module.exports = { apiGroups };
